d1cbd5aaf2ce933982d2558b189d44912a9c9cd8
Raiyan <raiyan.sharif.1234@gmail.com>
2020-06-29 02:17:41 +0600

    Project init

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by raiyan sharif on 29/6/20.
+//  Copyright © 2020 raiyansharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
